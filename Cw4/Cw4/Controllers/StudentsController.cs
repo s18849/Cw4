48c99ae8a274ce135fec84c9db41e06fb5222343
s18849 <s18849@pjwstk.edu.pl>
2020-03-26 12:10:14 +0100

    connection i command

---
+++ Cw4/Cw4/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw4.Controllers
+{
+    [ApiController]
+    [Route("api/students")]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            //SqlConnection con = new SqlConnection();
+            //SqlCommand com = new SqlCommand();
+            return Ok();
+        }
+    }
+}
