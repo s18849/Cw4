e490a6c51a0c5ebeb30eda1383bfd92519805127
s18849 <s18849@pjwstk.edu.pl>
2020-03-26 12:11:20 +0100

    zapisane connection i command

--- Cw4/Cw4/Controllers/StudentsController.cs
+++ Cw4/Cw4/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -10,11 +11,20 @@ namespace Cw4.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=s18849;Integrated Security=True";
         [HttpGet]
         public IActionResult GetStudents()
         {
-            //SqlConnection con = new SqlConnection();
-            //SqlCommand com = new SqlCommand();
+            using (var con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select * from students";
+            }
+            
+            
+
+            
             return Ok();
         }
     }
